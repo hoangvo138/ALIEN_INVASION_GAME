aaf5adaf0cb5c042819b0c29c25fdc2e6a69f5fa
Hoang Vo <hvo.stone@gmail.com>
2024-12-29 18:57:26 -0500

    Refactored run_game() method in alien_invasion.py file
    Refactored the run_game() method into two helper methods that can check keypress and mouse events, and update images on the screen.

--- alien_invasion.py
+++ alien_invasion.py
@@ -23,19 +23,25 @@ class AlienInvasion:
     def run_game(self):
         """Start the main loop for the game."""
         while True:
-            # Watch for keyboard and mouse events
-            for event in pygame.event.get():
+            self._check_events()
+            self._update_screen()
+            self.clock.tick(60)
+                    
+    def _check_events(self):
+        """Respond to keypresses and mouse events."""
+        for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     sys.exit()
                     
-            # Redraw the screen during each pass through the loop
-            # Draw the ship on the screen
-            self.screen.fill(self.settings.bg_color)
-            self.ship.blitme()
-                    
-            # Make the most recently drawn screen visible
-            pygame.display.flip()
-            self.clock.tick(60)
+    def _update_screen(self):
+        """Update images on the screen, and flip to the new screen"""
+        # Redraw the screen during each pass through the loop
+        # Draw the ship on the screen
+        self.screen.fill(self.settings.bg_color)
+        self.ship.blitme()
+                
+        # Make the most recently drawn screen visible
+        pygame.display.flip()
             
 
 if __name__ == '__main__':
