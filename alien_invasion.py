0a7c0468b616f3e75b50ce5b98320501bc57318a
Hoang Vo <hvo.stone@gmail.com>
2024-12-30 17:12:41 -0500

    modified alien_invasion.py and ship.py
    In ship.py, added moving_right flag, added update() method to update the ship's position when moving right. In alien_invasion.py, modified _check_events() method to respond when user presses the right arrow key, added ship.update() method to while loop to update ship's position after checking for events and before updating the screen.

--- alien_invasion.py
+++ alien_invasion.py
@@ -24,6 +24,7 @@ class AlienInvasion:
         """Start the main loop for the game."""
         while True:
             self._check_events()
+            self.ship.update()
             self._update_screen()
             self.clock.tick(60)
                     
@@ -32,15 +33,21 @@ class AlienInvasion:
         for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     sys.exit()
+                elif event.type == pygame.KEYDOWN:
+                    # move the ship to the right
+                    if event.key == pygame.K_RIGHT:
+                        self.ship.moving_right = True
+                elif event.type == pygame.KEYUP:
+                    if event.key == pygame.K_RIGHT:
+                        self.ship.moving_right = False
                     
     def _update_screen(self):
         """Update images on the screen, and flip to the new screen"""
         # Redraw the screen during each pass through the loop
         # Draw the ship on the screen
+        # Make the most recently drawn screen visible
         self.screen.fill(self.settings.bg_color)
         self.ship.blitme()
-                
-        # Make the most recently drawn screen visible
         pygame.display.flip()
             
 
